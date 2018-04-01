3d864e480d9efcfb63d7a3637e3eb5b8fbe19df2
huguangjing211 <huguangjingcome@126.com>
2018-04-01 22:02:28 +0800

    version

---
+++ config.py
@@ -0,0 +1,61 @@
+# -*- coding:utf-8 -*-
+
+
+import redis
+
+
+class Config(object):
+    """加载配置"""
+
+    # 开启调试模式
+    DEBUG = True
+
+    # 秘钥
+    SECRET_KEY = 'AK0j4NSomJQKm8gD/917OniOIC8DEMQRP+xPBvGanEBieaADMBTA0EBTrJdAiXgU'
+
+    # 配置mysql数据库:开发中使用真实IP
+    SQLALCHEMY_DATABASE_URI = 'mysql://root:mysql@192.168.145.128:3306/iHome_07'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    # 配置redis数据库：开发中使用真实IP
+    REDIS_HOST = '192.168.145.128'
+    REDIS_PORT = 6379
+
+    # 配置session数据存储到redis数据库
+    SESSION_TYPE = 'redis'
+    # 指定存储session数据的redis的位置
+    SESSION_REDIS = redis.StrictRedis(host=REDIS_HOST, port=REDIS_PORT)
+    # 开启session数据的签名，意思是让session数据不以明文形式存储
+    SESSION_USE_SIGNER = True
+    # 設置session的会话的超时时长 ：一天
+    PERMANENT_SESSION_LIFETIME = 3600 * 24
+
+
+class DevelopmentConfig(Config):
+    """创建调试环境下的配置类"""
+    # 我们的爱家租房的房型，调试模式的配置和Config一致，所以pass
+    pass
+
+
+class ProductionConfig(Config):
+    """创建线上环境下的配置类"""
+
+    # 重写有差异性的配置
+    SQLALCHEMY_DATABASE_URI = 'mysql://root:mysql@192.168.72.77:3306/iHome'
+
+
+class UnittestConfig(Config):
+    """单元测试的配置"""
+
+    # 重写有差异性的配置
+    SQLALCHEMY_DATABASE_URI = 'mysql://root:mysql@192.168.145.128:3306/iHome_testcast_07'
+
+
+
+# 准备工厂设计模式的原材料
+configs =  {
+    'default_config':Config,
+    'development':DevelopmentConfig,
+    'production':ProductionConfig,
+    'unittest':UnittestConfig
+}
---
+++ iHome/__init__.py
@@ -0,0 +1,54 @@
+# -*- coding:utf-8 -*-
+
+
+import redis
+from flask import Flask
+# session在flask中的扩展包
+from flask_session import Session
+from flask_sqlalchemy import SQLAlchemy
+from flask_wtf.csrf import CSRFProtect
+from config import configs
+from iHome.utils.common import RegexConverter
+
+
+# 创建可以被外界导入的数据库连接对象
+db = SQLAlchemy()
+# 创建可以被外界导入的连接到redis数据库的对象
+redis_store = None
+
+
+# default_config == config_name
+def get_app(config_name):
+    """工厂方法：根据不同的配置信息，实例化出不同的app"""
+
+    app = Flask(__name__)
+
+    # 加载配置
+    app.config.from_object(configs[config_name])
+
+    # 创建连接到mysql数据库的对象
+    # db = SQLAlchemy(app)
+    db.init_app(app)
+
+    # 创建连接到redis数据库的对象
+    global redis_store
+    redis_store = redis.StrictRedis(host=configs[config_name].REDIS_HOST, port=configs[config_name].REDIS_PORT)
+
+    # 开启CSRF保护
+    CSRFProtect(app)
+
+    # 使用session在flask扩展实现将session数据存储在redis
+    Session(app)
+
+    # 需要现有路由转换器，后面html_blue中才可以直接匹配
+    app.url_map.converters['re'] = RegexConverter
+
+    # 注册蓝图：为了解决导入api时，还没有redis_store，造成的ImportError: cannot import name redis_store
+    from iHome.api_1_0 import api
+    app.register_blueprint(api)
+
+    # 注册静态html文件加载时的蓝图
+    from iHome.web_html import html_blue
+    app.register_blueprint(html_blue)
+
+    return app
